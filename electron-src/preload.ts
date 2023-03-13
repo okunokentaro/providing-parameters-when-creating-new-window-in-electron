e01316036400a8303b0e6068463438ca023bc9e9
OKUNOKENTARO <okunokentaro@me.com>
2023-03-13 16:10:41 +0900

    contextIsolation: true

--- electron-src/preload.ts
+++ electron-src/preload.ts
@@ -1,5 +1,20 @@
-import { ipcRenderer } from 'electron'
+import { ipcRenderer, contextBridge } from "electron";
 
-process.once('loaded', () => {
-  (global as any).ipcRenderer = ipcRenderer
-})
+type Tail<T> = T extends [unknown, ...infer REST] ? REST : never;
+
+contextBridge.exposeInMainWorld("ipcRenderer", {
+  send: (
+    channel: Parameters<typeof ipcRenderer.send>[0],
+    ...args: Tail<Parameters<typeof ipcRenderer.send>>
+  ): ReturnType<typeof ipcRenderer.send> => {
+    return ipcRenderer.send(channel, ...args);
+  },
+  on: (
+    channel: Parameters<typeof ipcRenderer.on>[0],
+    listener: Parameters<typeof ipcRenderer.on>[1],
+    ...args: Tail<Tail<Parameters<typeof ipcRenderer.on>>>
+  ): () => ReturnType<typeof ipcRenderer.removeListener> => {
+    ipcRenderer.on(channel, listener, ...args);
+    return () => ipcRenderer.removeListener(channel, listener);
+  },
+});
--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,22 +1,17 @@
-import { useEffect } from 'react'
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { useEffect, useCallback } from "react";
+import Link from "next/link";
+import Layout from "../components/Layout";
 
 const IndexPage = () => {
   useEffect(() => {
-    const handleMessage = (_event, args) => alert(args)
+    const handleMessage = (_event, args) => alert(args);
+    const dispose = globalThis.ipcRenderer.on("message", handleMessage);
+    return () => dispose();
+  }, []);
 
-    // add a listener to 'message' channel
-    global.ipcRenderer.addListener('message', handleMessage)
-
-    return () => {
-      global.ipcRenderer.removeListener('message', handleMessage)
-    }
-  }, [])
-
-  const onSayHiClick = () => {
-    global.ipcRenderer.send('message', 'hi from next')
-  }
+  const onSayHiClick = useCallback(() => {
+    globalThis.ipcRenderer.send("message", "hi from next");
+  }, []);
 
   return (
     <Layout title="Home | Next.js + TypeScript + Electron Example">
@@ -26,7 +21,7 @@ const IndexPage = () => {
         <Link href="/about">About</Link>
       </p>
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
